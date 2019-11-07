9dff277ee9799351bce4ff0d5b607138e1c61047
zrm <623654589@qq.com>
2019-11-08 03:45:35 +0800

    commit101

--- leetcode/src/main/java/com/leetcode101/Solution.java
+++ leetcode/src/main/java/com/leetcode101/Solution.java
@@ -4,5 +4,29 @@ package com.leetcode101;
  * @author zack
  * @create 2019-11-08-3:02
  */
-public class Solution {
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
 }
