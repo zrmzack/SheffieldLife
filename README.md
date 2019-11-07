35b2fe65ab0cc1fca7e233afc85e4136ccc32dc7
zrm <623654589@qq.com>
2019-11-08 04:58:38 +0800

    leetcode&&readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SheffieldLife
 This is the life in Sheffield
+
+This contain the android, Machine Learning, JAVA, Text Processing
--- leetcode/src/main/java/com/leetcode101/Solution.java
+++ leetcode/src/main/java/com/leetcode101/Solution.java
@@ -4,23 +4,6 @@ package com.leetcode101;
  * @author zack
  * @create 2019-11-08-3:02
  */
-
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-
-    }
-}
-
 class TreeNode {
     int val;
     TreeNode left;
@@ -28,5 +11,19 @@ class TreeNode {
 
     TreeNode(int x) {
         val = x;
+    }
+}
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return (t1.val == t2.val)
+                && isMirror(t1.right, t2.left)
+                && isMirror(t1.left, t2.right);
     }
 }
