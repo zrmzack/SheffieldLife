76e96ac730bec2b7ed056c92dfae034e40d09515
zrm <623654589@qq.com>
2019-11-11 03:59:43 +0800

    change

--- JAVA/src/main/java/Lab2/GenerateWalkingPlan.java
+++ JAVA/src/main/java/Lab2/GenerateWalkingPlan.java
@@ -6,14 +6,15 @@ import java.util.Scanner;
  * @author zack
  * @create 2019-11-11-3:09
  */
-public class GenerateWalikingPlan {
+public class GenerateWalkingPlan {
     public static void main(String[] args) {
         System.out.print("What is your name? ");
         String name = new Scanner(System.in).nextLine();
         System.out.println();
         System.out.print("Hello " + name + ";");
         System.out.print(" how old are you?");
-        int age=100;
+        int age = 100;
+        //If input wrong age will be 100
         try {
             age = new Scanner(System.in).nextInt();
         } catch (Exception e) {
