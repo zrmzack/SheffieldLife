2173be4d8a724c8d96632b26c71a7d19dcd387c1
zrm <623654589@qq.com>
2019-11-18 02:21:36 +0800

    updateleetcode

--- leetcode/src/main/java/com/leetcode107/Solution.java
+++ leetcode/src/main/java/com/leetcode107/Solution.java
@@ -1,5 +1,6 @@
 package com.leetcode107;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,8 +18,24 @@ class TreeNode {
     }
 }
 
-public class Solution {
+class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-
+        List<List<Integer>> result = new ArrayList();
+        List<TreeNode> list = new ArrayList();
+        if (root == null) return result;
+        list.add(root);
+        while (!list.isEmpty()) {
+            List<Integer> curlist = new ArrayList();
+            List<TreeNode> nextlist = new ArrayList();
+            for (TreeNode cur : list) {
+                curlist.add(cur.val);
+                if (cur.left != null) nextlist.add(cur.left);
+                if (cur.right != null) nextlist.add(cur.right);
+            }
+            list = nextlist;
+            result.add(0, curlist); // 先近先出
+        }
+        return result;
     }
+
 }
