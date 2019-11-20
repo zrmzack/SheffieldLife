64d2af580aed9cdbbce194ccc8715ca044af4518
zrm <623654589@qq.com>
2019-11-20 21:28:20 +0800

    java ml

---
+++ leetcode/src/main/java/com/leetcode122/Solution.java
@@ -0,0 +1,8 @@
+package com.leetcode122;
+
+/**
+ * @author zack
+ * @create 2019-11-19-17:08
+ */
+public class Solution {
+}
---
+++ leetcode/src/main/java/com/leetcode167/Solution.java
@@ -0,0 +1,8 @@
+package com.leetcode167;
+
+/**
+ * @author zack
+ * @create 2019-11-19-19:12
+ */
+public class Solution {
+}
