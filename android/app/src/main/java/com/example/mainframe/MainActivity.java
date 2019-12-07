2748d44989c32d53b9685eef4d1039b4b344687c
zrm <623654589@qq.com>
2019-12-08 05:10:12 +0800

    mainframe

---
+++ android/app/src/main/java/com/example/mainframe/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mainframe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
