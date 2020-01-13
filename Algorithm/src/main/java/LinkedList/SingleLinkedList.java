22fc756c6fb794265a2ee906f30c6c39b865be1d
zrm <623654589@qq.com>
2020-01-14 01:00:37 +0800

    zrm

--- Algorithm/src/main/java/LinkedList/SingleLinkedList.java
+++ Algorithm/src/main/java/LinkedList/SingleLinkedList.java
@@ -16,6 +16,62 @@ public class SingleLinkedList {
         singleLinkedListTest.add(heroNode3);
         singleLinkedListTest.add(heroNode4);
         singleLinkedListTest.list();
+        singleLinkedListTest.del(1);
+        singleLinkedListTest.list();
+
+        System.out.println("有效节点的个数" + getLength(singleLinkedListTest.getHead()));
+    }
+
+    //获取到单链表的节点个数
+    public static int getLength(HeroNode head) {
+        if (head.next == null) {
+            return 0;
+        }
+        int length = 0;
+        HeroNode temp = head.next;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        return length;
+    }
+
+    /*
+     * 查找链表的第k个元素
+     * 首先获取链表有效节点的个数
+     * */
+    public static HeroNode finaLastIndexNode(HeroNode head, int index) {
+        if (head.next == null) {
+            return null;
+        }
+        int size = getLength(head);
+        if (index < 0 || index > size) {
+            return null;
+        }
+        HeroNode temp = head.next;
+        for (int i = 0; i < size - index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    /*
+     *
+     * 链表反转(百度面试题目)
+     * */
+    public static void reversetList(HeroNode head) {
+        if (head.next == null || head.next.next == null) {
+            return;
+        }
+        HeroNode temp = head.next;//定义temp只想正真数据的第一个
+        HeroNode next = null;//指向temp节点的后一个
+        HeroNode reverseHead = new HeroNode(0, "", "");
+        while (temp != null) {
+            next = temp.next;//保存当前节点的下一个节点
+            temp.next = reverseHead.next;//把temp的下一个jie'd节点指向新的链表的最前端
+            temp=next;//temp 往后移动
+        }
+        head.next=reverseHead.next;
     }
 }
 
@@ -44,8 +100,16 @@ class HeroNode {
 }
 
 class SingleLinkedListTest {
-    //头节点不存数据
+    //头节点不存数据(后新建一个temp节点来替代head节点)
     private HeroNode head = new HeroNode(0, "", "");
+
+    public HeroNode getHead() {
+        return head;
+    }
+
+    public void setHead(HeroNode head) {
+        this.head = head;
+    }
 
     public void add(HeroNode heroNode) {
         //生成一个辅助节点
@@ -60,6 +124,82 @@ class SingleLinkedListTest {
         }
         //将最后的节点next指向了新的节点
         temp.next = heroNode;
+    }
+
+    public void addByOrder(HeroNode heroNode) {
+        HeroNode temp = head;
+        boolean flag = false;//该节点是否存在
+        while (true) {
+            if (temp.next == null) {
+                break;
+            }
+            if (temp.next.no > heroNode.no) {//可以插入
+
+
+                break;
+            } else if (temp.next.no == heroNode.no) {
+                flag = true;
+                break;
+            }
+
+            temp = temp.next;
+        }
+
+        if (flag == true) {
+            System.out.println("该点已经存在" + heroNode.no);
+        } else {
+            heroNode.next = temp.next;
+            temp.next = heroNode;
+        }
+
+    }
+
+    public void update(HeroNode newHeroNode) {
+        if (head.next == null) {
+            System.out.println("链表为空");
+            return;
+        }
+        HeroNode temp = head.next;
+        boolean flag = false;
+        while (true) {
+            if (temp == null) {
+                break;//遍历完所有链表
+            }
+            if (temp.no == newHeroNode.no) {
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+
+        }
+        if (flag == true) {
+            temp.name = newHeroNode.name;
+            temp.nickname = newHeroNode.nickname;
+        } else {
+            System.out.println("没有找到");
+        }
+
+    }
+
+    public void del(int no) {
+        HeroNode temp = head;
+        boolean flag = false;//是否找到
+        while (true) {
+            if (temp.next == null) {
+                break;
+            }
+            if (temp.next.no == no) {
+                //找到了这个节点了
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag = true) {
+            temp.next = temp.next.next;
+        } else {
+            System.out.println("没找到");
+        }
     }
 
     public void list() {
@@ -81,4 +221,6 @@ class SingleLinkedListTest {
 
         }
     }
+
+
 }
